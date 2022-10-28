ab936742f61bb60f1db8610455d53f2f7b8d33e1
SAISH1789 <45946634+SAISH1789@users.noreply.github.com>
2022-10-27 17:41:10 -0700

    Added a skeleton code

---
+++ urinals.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.test;
+public class urinals {
+    // author- Saish Vemulapalli(svemul20@asu.edu)
+    public static void main(String[] args){
+        getString();
+        openFile();
+        countUrinals();
+    }
+    public static void getString(){
+        System.out.println("Not yet implemented");
+    }
+    public static void openFile(){
+        System.out.println("Not yet implemented");
+    }
+    public static void countUrinals(){
+        System.out.println("Not yet implemented");
+    }
+}
